71790e6f2e35532a7521ce6610aecdd9db850a50
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-03-15 18:43:11 -0500

    lint(model): implement correct indentation

--- src/model/userModel.ts
+++ src/model/userModel.ts
@@ -6,9 +6,9 @@ interface User {
 }
 
 const userCollection: User[] = [
-            { id: 1, name: 'Jhon Doe', email: 'jane@example.com' },
-            { id: 2, name: 'John Smith', email: 'john@example.com' }
-        ]
+    { id: 1, name: 'Jhon Doe', email: 'jane@example.com' },
+    { id: 2, name: 'John Smith', email: 'john@example.com' }
+]
 
 export function listUsers() {
     return userCollection;
