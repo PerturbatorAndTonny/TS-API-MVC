a78918edecbf54b686b7d383695b333bfbc12ff6
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-03-15 18:40:03 -0500

    feat(model): implement abstraction for model DB

---
+++ src/model/userModel.ts
@@ -0,0 +1,15 @@
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+}
+
+const userCollection: User[] = [
+            { id: 1, name: 'Jhon Doe', email: 'jane@example.com' },
+            { id: 2, name: 'John Smith', email: 'john@example.com' }
+        ]
+
+export function listUsers() {
+    return userCollection;
+}
